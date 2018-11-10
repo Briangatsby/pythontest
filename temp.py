1d88a7e96fe30e123ad6c9400f5d42feaf0f9b4f
Briangatsby <33981383+Briangatsby@users.noreply.github.com>
2018-11-10 22:31:00 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=1234
+b='hellow world'
+print(a)
+print(b)
+c=3756
+<<<<<<< HEAD
+print(c)
+=======
+print(c)
+d='a test file'
+print(d)
+e = 7777
+print(e)
+>>>>>>> 20181110_1
