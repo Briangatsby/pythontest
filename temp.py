6f6961614c32971cbb312e2c677f7de6e823fb7d
Briangatsby <33981383+Briangatsby@users.noreply.github.com>
2018-11-11 08:23:43 +0800

    no message

--- temp.py
+++ temp.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
@@ -19,3 +20,26 @@ print(d)
 e = 7777
 print(e)
 >>>>>>> 20181110_1
+=======
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=1234
+b='hellow world'
+print(a)
+print(b)
+c=3756
+<<<<<<< HEAD
+print(c)
+=======
+print(c)
+d='a test file'
+print(d)
+e = 7777
+print(e)
+>>>>>>> 20181110_1
+>>>>>>> no message
